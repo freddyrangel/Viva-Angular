2ca5bb67e2f45d581e98eee0b8bee62e0f7d0c11
Freddy <freddy01288@gmail.com>
2014-07-23 16:38:27 -0500

    agregamos eventSource a el control UserCalendarCtl

--- app/scripts/controllers/user-calendar-controller.js
+++ app/scripts/controllers/user-calendar-controller.js
@@ -1,6 +1,23 @@
 'use strict';
 
 app.controller('UserCalendarCtrl', function($scope) {
+	
+	var date = new Date(),
+  d = date.getDate(),
+  m = date.getMonth(),
+  y = date.getFullYear();
 
+	$scope.eventSource = {
+		    $http.get('http://162.243.222.54/fullcalendar/new_fechas_admin.php')
+        .success(function(data) {
+        });
+
+
+
+		// url: 'http://162.243.222.54/fullcalendar/new_fechas_admin.php'
+	};
+
+	console.log($scope.eventSource);
+	$scope.calendar.fullCalendar('addEventSource', source);
 
 });
