8d007af889259d94443d90616d5b858c38ebe1dd
Freddy Rangel <frederick.rangel@gmail.com>
2014-07-23 17:37:16 -0500

    added eventSources to user-calendar-controller.js

--- app/scripts/controllers/user-calendar-controller.js
+++ app/scripts/controllers/user-calendar-controller.js
@@ -1,23 +1,29 @@
 'use strict';
 
 app.controller('UserCalendarCtrl', function($scope) {
-	
-	var date = new Date(),
+  var date = new Date(),
   d = date.getDate(),
   m = date.getMonth(),
   y = date.getFullYear();
 
-	$scope.eventSource = {
-		    $http.get('http://162.243.222.54/fullcalendar/new_fechas_admin.php')
-        .success(function(data) {
-        });
+  $scope.uiConfig = {
+    calendar:{
+      height: 450,
+      editable: true,
+      header:{
+        left: 'month basicWeek basicDay agendaWeek agendaDay',
+        center: 'title',
+        right: 'today prev,next'
+      },
+      dayClick: $scope.alertEventOnClick,
+      eventDrop: $scope.alertOnDrop,
+      eventResize: $scope.alertOnResize
+    }
+  };
 
+  $scope.eventSources = {
+    url: 'http://162.243.222.54/fullcalendar/new_fechas_admin.php'
+  };
 
-
-		// url: 'http://162.243.222.54/fullcalendar/new_fechas_admin.php'
-	};
-
-	console.log($scope.eventSource);
-	$scope.calendar.fullCalendar('addEventSource', source);
-
+	//$scope.calendar.fullCalendar('addEventSource', source);
 });
